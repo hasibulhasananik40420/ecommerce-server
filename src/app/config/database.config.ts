8c7860c866eaaae3777bc918501b503b5c1513fd
Hasibul Hasan <macbook@Macbooks-MacBook-Pro.local>
2025-06-16 11:55:50 +0600

    added

--- src/app/config/database.config.ts
+++ src/app/config/database.config.ts
@@ -1,23 +1,46 @@
+// import mongoose, { ConnectOptions } from 'mongoose';
+// import config from '.';
+// import seedSuperAdmin from './DB';
+
+// export async function databaseConnecting() {
+//   try {
+//     if (config.mongo_prod as object | undefined) {
+//       await mongoose.connect(`${config.mongo_uri_prod}`, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//         serverSelectionTimeoutMS: 1000,
+//       } as ConnectOptions);
+//       seedSuperAdmin();
+//       console.log('Database      :🚀 Connected to database (Production)');
+//     } else {
+//       await mongoose.connect(config.mongo_uri_dev as string);
+//       seedSuperAdmin();
+//       console.log('Database      :🚀 Connected to database (Development)');
+//     }
+//   } catch (error) {
+//     console.error('Database      :🙄 Error connecting to the database');
+//   }
+// }
+
 import mongoose, { ConnectOptions } from 'mongoose';
 import config from '.';
 import seedSuperAdmin from './DB';
 
 export async function databaseConnecting() {
   try {
-    if (config.mongo_prod as object | undefined) {
-      await mongoose.connect(`${config.mongo_uri_prod}`, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 1000,
-      } as ConnectOptions);
-      seedSuperAdmin();
-      console.log('Database      :🚀 Connected to database (Production)');
-    } else {
-      await mongoose.connect(config.mongo_uri_dev as string);
-      seedSuperAdmin();
-      console.log('Database      :🚀 Connected to database (Development)');
-    }
+    const uri = config.mongo_prod ? config.mongo_uri_prod : config.mongo_uri_dev;
+
+    await mongoose.connect(uri as string, {
+      serverSelectionTimeoutMS: 1000,
+    } as ConnectOptions);
+
+    seedSuperAdmin();
+
+    console.log(
+      `Database      :🚀 Connected to database (${config.mongo_prod ? 'Production' : 'Development'})`
+    );
   } catch (error) {
     console.error('Database      :🙄 Error connecting to the database');
+    console.error('Error details :❌', (error as Error).message);
   }
 }
